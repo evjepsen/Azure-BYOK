d241fad1e931fd1cb5b27bd0553f8207d6223874
Emil Viftrup Jepsen <202204239@post.au.dk>
2025-03-17 09:38:53 +0100

    Add docs to audit service and controller

--- API/Controllers/AuditController.cs
+++ API/Controllers/AuditController.cs
@@ -3,25 +3,42 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+/// <summary>
+/// Controller to audit the operations performed on the Azure Vault instance and keys stored in it
+/// </summary>
 [Route("[controller]")]
 public class AuditController : Controller
 {
     private readonly IAuditService _auditService;
 
+    /// <summary>
+    /// The constructor for the controller
+    /// </summary>
+    /// <param name="auditService">The audit service used to access the logs</param>
     public AuditController(IAuditService auditService)
     {
         _auditService = auditService;
     }
 
+    /// <summary>
+    /// Get the key operations performed on all keys 
+    /// </summary>
+    /// <param name="numOfDays">The time period to get logs in days</param>
+    /// <returns>The key activity log entries for the specified period in JSON format</returns>
     [HttpGet("/keys/{numOfDays}")]
-    public async Task<IActionResult> AuditKeyOperationsPerformed(int numOfDays)
+    public async Task<IActionResult> GetKeyOperationsPerformed(int numOfDays)
     {
         var res = await _auditService.GetKeyOperationsPerformedAsync(numOfDays);
         return Ok(res);
     }
     
+    /// <summary>
+    /// Get the operations performed on the vault
+    /// </summary>
+    /// <param name="numOfDays">The time period to get logs in days</param>
+    /// <returns>The vault activity log entries for the specified period in JSON format</returns>
     [HttpGet("/vault/{numOfDays}")]
-    public async Task<IActionResult> AuditVaultOperationsPerformed(int numOfDays)
+    public async Task<IActionResult> GetVaultOperationsPerformed(int numOfDays)
     {
         var res = await _auditService.GetVaultOperationsPerformedAsync(numOfDays);
         return Ok(res);
