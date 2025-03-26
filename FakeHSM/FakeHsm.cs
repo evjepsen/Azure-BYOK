6c65b485d6cd59d6f295cb98cd00859c876a54db
smavl <donsmavl@protonmail.com>
2025-03-26 14:25:51 +0100

    removed excessive features,

--- FakeHSM/FakeHsm.cs
+++ FakeHSM/FakeHsm.cs
@@ -7,104 +7,34 @@ using Org.BouncyCastle.Crypto.Parameters;
 
 namespace FakeHSM;
 
-public enum Algorithm
-{
-    RSA,
-    AES
-}
-
-public class KeyProperties(Algorithm algorithm, int keySize) 
-{
-    public Algorithm Algorithm { get; set; } = algorithm;
-    public int KeySize { get; set; } = keySize;
-}
-
-public class KeyEntry(Object keyObject, Algorithm algorithm, int keySize)
-{
-    internal Object KeyObject { get; set; } = keyObject;
-    public KeyProperties KeyProperties { get; set; } = new(algorithm, keySize);
-}
 
 public class FakeHsm : IFakeHsm
 {
-    private Dictionary<string,KeyEntry> _keys;
-    
-    public FakeHsm()
-    {
-        _keys = new Dictionary<string, KeyEntry>();
-    }
 
-    public (string randomId, byte[] pk) GenerateRsaKey(int bitLength)
+    private static byte[] GeneratePrivateRsaKey(int bitLength)
     {
         var rsa = RSA.Create(bitLength);
-        var pk = rsa.ExportSubjectPublicKeyInfo();
-        var keyEntry = new KeyEntry(rsa, Algorithm.RSA, bitLength);
-        var randomId = Guid.NewGuid().ToString();
-        _keys.Add(randomId, keyEntry);
-        return (randomId, pk);
+        var sk = rsa.ExportPkcs8PrivateKey();
+        return sk;
     }
-    public string GenerateAesKey(int bitLength)
+    private static byte[] GenerateAesKey(int bitLength)
     {
         // Generate the AES key
         var aes = Aes.Create();
         aes.KeySize = bitLength;
         aes.GenerateKey();
-        var keyEntry = new KeyEntry(aes, Algorithm.AES, bitLength);
-        var randomId = Guid.NewGuid().ToString();
-        _keys.Add(randomId, keyEntry);
-        return (randomId);
-    }
-
-    public byte[] GetPublicKeyOfId(string id)
-    {
-        if (!_keys.TryGetValue(id, out KeyEntry? keyEntry))
-        {
-            throw new KeyNotFoundException();
-        }
-        if (GetKeyProperties(id).Algorithm != Algorithm.RSA) throw new Exception("Key is not an RSA key");
-
-        var rsa = (RSA)keyEntry.KeyObject;
-        return rsa.ExportSubjectPublicKeyInfo();
-    }
-    
-    // get the ids of the keys
-    public List<string> GetKeyIds()
-    {
-        return _keys.Keys.ToList();
-    }
-
-    public KeyProperties GetKeyProperties(string id)
-    {
-        return _keys[id].KeyProperties;
-    }
-
-    public byte[] EncryptWithKey(string id, byte[] data)
-    {
-        // check if the key exists
-        if (!_keys.TryGetValue(id, out KeyEntry? keyEntry))
-        {
-            throw new KeyNotFoundException();
-        }
-        
-        var keyObject = keyEntry.KeyObject;
-        // Encrypt the AES key using the KEK using RSA-OAEP with SHA1
-        if (GetKeyProperties(id).Algorithm != Algorithm.RSA) throw new Exception("The key with that id is not an RSA key");
-        var rsa = (RSA)keyObject;
-        return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA1);
+        return aes.Key;
     }
 
     public byte[] GenerateBlob(KeyVaultKey kek)
     {
         // Generate the customer's private key
-        var (id ,_) = GenerateRsaKey(2048);
-        var pk = GetPrivateKey(id);
-        
+        var sk = GeneratePrivateRsaKey(2048);
         // Convert the KEK to an RSA object
         var rsa = kek.Key.ToRSA();
         
         // Generate the AES key
-        var aesKeyId = GenerateAesKey(256);
-        var aesKey = GetAesKey(aesKeyId);
+        var aesKey= GenerateAesKey(256);
         
         // Encrypt the AES key using the KEK using RSA-OAEP with SHA1
         var encryptedAesKey = rsa.Encrypt(aesKey, RSAEncryptionPadding.OaepSHA1);
@@ -122,11 +52,11 @@ public class FakeHsm : IFakeHsm
         ms.Write(aes.IV, 0, aes.IV.Length);
         using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
         {
-            cs.Write(pk, 0, pk.Length);
+            cs.Write(sk, 0, sk.Length);
             cs.FlushFinalBlock();
         }
             
-        var encryptedKeyMaterial = AesKeyWrapWithPadding(pk, aesKey);
+        var encryptedKeyMaterial = AesKeyWrapWithPadding(sk, aesKey);
         
         // The Aes key and key material (both encrypted) are concatenated to produce the ciphertext
         var ciphertext = new byte[encryptedAesKey.Length + encryptedKeyMaterial.Length];
@@ -136,34 +66,6 @@ public class FakeHsm : IFakeHsm
         return ciphertext;
     }
 
-    private byte[] GetPrivateKey(string id)
-    {
-        // check if the key exists
-        if (!_keys.TryGetValue(id, out KeyEntry? keyEntry))
-        {
-            throw new KeyNotFoundException();
-        }
-        // check if the key is an RSA key
-        if (GetKeyProperties(id).Algorithm != Algorithm.RSA) throw new Exception("Key is not an RSA key");
-
-        var rsa = (RSA)keyEntry.KeyObject;
-        return rsa.ExportPkcs8PrivateKey();
-    }
-
-    private byte[] GetAesKey(string id)
-    {
-        // check if the key exists
-        if (!_keys.TryGetValue(id, out var keyEntry))
-        {
-            throw new KeyNotFoundException();
-        }
-        // check if the key is an AES key
-        if (GetKeyProperties(id).Algorithm != Algorithm.AES) throw new Exception("Key is not an AES key");
-
-        var aes = (Aes)keyEntry.KeyObject;
-        return aes.Key;
-    }
-    
     private byte[] AesKeyWrapWithPadding(byte[] keyToWrap, byte[] aesKey)
     {
         IWrapper wrapper = new AesWrapPadEngine();
--- Test/FakeHsmTest.cs
+++ Test/FakeHsmTest.cs
@@ -1,3 +1,4 @@
+using FakeHSM.Interfaces;
 using Infrastructure;
 using Test.TestHelpers;
 using FakeHsm = FakeHSM.FakeHsm;
@@ -8,7 +9,7 @@ namespace Test;
 [TestOf(typeof(FakeHsm))]
 public class FakeHsmTest
 {
-    private FakeHsm _fakeHsm;
+    private IFakeHsm _fakeHsm;
 
     [SetUp]
     public void Setup()
@@ -16,59 +17,21 @@ public class FakeHsmTest
         _fakeHsm = new FakeHsm();
     }
     [Test]
-    public void HsmShouldBeEmptyAfterInit()
-    {
-        // given a newly initialized HSM
-        
-        // when I ask for the number of keys
-        var numOfKeys = _fakeHsm.GetKeyIds().Count;
-        
-        // then it should be empty
-        Assert.That(numOfKeys, Is.Zero);
-    }
-    [Test]
-    public void HsmShouldHaveOneKeyAfterKeyGeneration()
-    {
-        _fakeHsm.GenerateRsaKey(2048);
-        // given a hsm with one key
-        
-        // when I ask for the number of keys
-        var numOfKeys = _fakeHsm.GetKeyIds().Count;
-        
-        // then it should have one key
-        Assert.That(numOfKeys, Is.EqualTo(1));
-    }
-    [Test]
-    public void ShouldBeAbleToGetPublicKeyWithId()
-    {
-        var (id, wantPk) = _fakeHsm.GenerateRsaKey(2048);
-        // given a hsm with one key
-        
-        // when I ask for the public key of the key
-        var gotPk = _fakeHsm.GetPublicKeyOfId(id);
-        
-        // then it should be the same as the one I generated
-        Assert.That(gotPk, Is.EqualTo(wantPk));
-    }
-
-    [Test]
     public async Task ShouldBlobBeGenerated()
     {
         TestHelper.CreateTestConfiguration();
-        var _tokenService = new TokenService();
+        var tokenService = new TokenService();
         IHttpClientFactory httpClientFactory = new FakeHttpClientFactory();
-        var _configuration = TestHelper.CreateTestConfiguration();
-        var _keyVaultService = new KeyVaultService(_tokenService, httpClientFactory,_configuration);
+        var configuration = TestHelper.CreateTestConfiguration();
+        var keyVaultService = new KeyVaultService(tokenService, httpClientFactory,configuration);
         // Given a key vault service
         
         var kekName = $"KEK-{Guid.NewGuid()}";
-        var kek = await _keyVaultService.GenerateKekAsync(kekName);
-        var hsm = new FakeHsm();
-        // var newKeyName = $"customer-KEY-{Guid.NewGuid()}";
+        var kek = await keyVaultService.GenerateKekAsync(kekName);
         // Given a Key Encryption Key 
         
         // When I ask to generate a blob
-        var transferBlob = hsm.GenerateBlob(kek);
+        var transferBlob = _fakeHsm.GenerateBlob(kek);
         
         // Then it should be successful
         Assert.That(transferBlob, Is.Not.Null);
