6ce28d7e765adf5688c2e10c518079a950c127eb
Emil Viftrup Jepsen <202204239@post.au.dk>
2025-04-04 10:07:44 +0200

    Add unit tests to test new method

--- Test/TestKeyVaultService.cs
+++ Test/TestKeyVaultService.cs
@@ -178,4 +178,27 @@ public class TestKeyVaultService
         // Then it should
         Assert.That(keyExists, Is.False);
     }
+
+    [Test]
+    public void ShouldAllowedOperationsBeValid()
+    {
+        // Given a list of key operations
+        string[] operations = ["encrypt", "decrypt", "sign", "verify", "wrapKey", "unwrapKey"];
+        // When I ask whether they are valid
+        var result = _keyVaultService.ValidateKeyOperations(operations);
+        // Then they should be valid
+        Assert.That(result.IsValid, Is.True);
+    } 
+    
+    [Test]
+    public void ShouldNotAllowedOperationsBeInvalid()
+    {
+        // Given a list of key operations
+        string[] operations = ["encrypt", "decrypt", "sign", "abc"];
+        // When I ask whether they are valid
+        var result = _keyVaultService.ValidateKeyOperations(operations);
+        // Then they should be valid
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.ErrorMessage, Is.EqualTo("Invalid key operations detected: abc"));
+    } 
 }
