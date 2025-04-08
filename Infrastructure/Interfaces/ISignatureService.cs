2a78daec8d50257ad2bf4713765043e4bd9238cc
Emil Viftrup Jepsen <202204239@post.au.dk>
2025-04-08 12:16:40 +0200

    Add comments on signature service

--- Infrastructure/Interfaces/ISignatureService.cs
+++ Infrastructure/Interfaces/ISignatureService.cs
@@ -1,8 +1,23 @@
 namespace Infrastructure.Interfaces;
 
+/// <summary>
+/// Service used to verify the customer's signature
+/// </summary>
 public interface ISignatureService
 {
+    /// <summary>
+    /// Check whether the signature given is valid
+    /// </summary>
+    /// <param name="signatureBase64">The signature in base64</param>
+    /// <param name="data">The signed data</param>
+    /// <returns>True if the signature is valid</returns>
     public bool IsSignatureValid(string signatureBase64, byte[] data);
 
+    /// <summary>
+    /// Get the signed data using the key data and the timestamp
+    /// </summary>
+    /// <param name="keyData">The data on the key</param>
+    /// <param name="timeStamp">The timestamp of sending the request</param>
+    /// <returns>A byte array containing the signed data</returns>
     public byte[] GetSignedData(byte[] keyData, DateTime timeStamp);
 }
