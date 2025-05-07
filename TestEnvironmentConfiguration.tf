8839af14bdbb60d0d94b61863c6499270414f905
Emil Viftrup Jepsen <202204239@post.au.dk>
2025-05-07 13:48:51 +0200

    update terraform config

--- TestEnvironmentConfiguration.tf
+++ TestEnvironmentConfiguration.tf
@@ -314,6 +314,25 @@ resource "azurerm_monitor_diagnostic_setting" "kv_logs" {
   }
 }
 
+resource "azurerm_monitor_diagnostic_setting" "activity_logs" {
+  name                       = "kv_activity_logs"
+  target_resource_id         = azurerm_key_vault.vault.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.ai.id
+
+  enabled_log {
+    category = "allLogs"
+  }
+
+  enabled_log {
+    category_group = "audit"
+  }
+
+
+  metric {
+    category = "AllMetrics"
+  }
+}
+
 # Output the password
 output "admin_password" {
   sensitive = true
